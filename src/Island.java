90a876db3b27afb2929e701680f55d7b8ca39951
”Pavlo <levkopavlo@gmail.com>
2023-09-03 00:43:06 +0300

    add Object [][] of island

--- src/Island.java
+++ src/Island.java
@@ -5,32 +5,7 @@ import java.util.List;
 public class Island {
     private int widthIsland;
     private int lengthIsland;
-    int[][] sectionOfTheIsland;
+    Object[][] sectionOfTheIsland;
     ListOfAnimals listOfAnimals = new ListOfAnimals();
-    public Island(int widthIsland, int lengthIsland) {
-        this.widthIsland = widthIsland;
-        this.lengthIsland = lengthIsland;
-    }
-    public int getLengthIsland() {
-        return lengthIsland;
-    }
-    public int getWidthIsland() {
-        return widthIsland;
-    }
-    public void fillAnimalsToArray() {
-        sectionOfTheIsland = new int[widthIsland][lengthIsland];
-        for (int i = 0; i < lengthIsland-1; i++) {
-            for (int j = 0; j < widthIsland-1; j++) {
-                sectionOfTheIsland[i][j] = listOfAnimals.createRandomAnimal();
-            }
-        }
-    }
 
-    public void getAnimalsArray() {
-        for (int i = 0; i < lengthIsland; i++) {
-            for (int j = 0; j < widthIsland; j++) {
-                System.out.println( " 5 " + sectionOfTheIsland[i][j]);
-            }
-        }
-    }
 }
